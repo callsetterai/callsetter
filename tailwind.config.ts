ed54067f395c3867132f0ce240d349d752e99b01
callsetterai <vicsmush@gmail.com>
2025-12-17 18:07:10 -0400

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,5 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+export default {
   content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
   theme: { extend: {} },
   plugins: []
-};
-
-export default config;
+}
