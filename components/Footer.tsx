ed73aaf5be171b20aa546f6cbaab69bbdffe1e42
callsetterai <vicsmush@gmail.com>
2025-12-23 06:13:56 -0400

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className="mt-10 border-t border-white/10 pt-8 text-center text-xs text-white/50">
+      © {new Date().getFullYear()} CallSetterAI. All rights reserved.
+    </div>
+  );
+}
